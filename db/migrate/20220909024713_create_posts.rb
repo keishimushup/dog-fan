eac52fecad18b4f945425fc9ee31bfc16600a8ec
keishimushup <108566690+keishimushup@users.noreply.github.com>
2022-09-09 03:09:27 +0000

    add model

---
+++ db/migrate/20220909024713_create_posts.rb
@@ -0,0 +1,13 @@
+class CreatePosts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :posts do |t|
+      
+      t.integer :user_id
+      t.integer :genre_id
+      t.string :title
+      t.text :explanation
+
+      t.timestamps
+    end
+  end
+end
