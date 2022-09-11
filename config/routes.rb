5cf2714178c44779518ba70805d27334542a40af
keishimushup <108566690+keishimushup@users.noreply.github.com>
2022-09-11 10:14:29 +0000

    update ユーザー情報

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,22 @@
 Rails.application.routes.draw do
-  scope module: :user do
-    root to: "homes#top"
-    resources :posts, only: [:new, :create, :index, :show, :edit, :update, :destroy]
-  end
   #ユーザー側
   devise_for :users,skip: [:passwords], controllers: {
   registrations: "user/registrations",
   sessions: 'user/sessions'
   }
+
   #管理者側
   devise_for :admin,skip: [:passwords], controllers: {
   sessions: "admin/sessions",
   registrations: "admin/registrations"
   }
+
+  scope module: :user do
+    root to: "homes#top"
+    resources :users, only: [:show, :edit, :update]
+    resources :posts, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+  end
+
+
 
 end
