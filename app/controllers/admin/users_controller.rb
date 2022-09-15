06d8bc72cdaf6dd1a74b0a97937fbda05d5b055b
keishimushup <108566690+keishimushup@users.noreply.github.com>
2022-09-15 06:46:03 +0000

    add admin側ユーザー削除機能追加

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,4 +3,14 @@ class Admin::UsersController < ApplicationController
     @user = User.find(params[:id])
     @posts = @user.posts
   end
+
+  def confilm
+    @user = User.find(params[:id])
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    redirect_to admin_root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -28,6 +28,7 @@ Rails.application.routes.draw do
 
   namespace :admin do
     root to: "homes#top"
-    resources :users, only: [:show]
+    resources :users, only: [:show, :destroy]
+    get "/users/confilm/:id" => "users#confilm"
   end
 end
