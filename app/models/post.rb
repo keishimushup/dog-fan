6c256518595928689a5f875462e976fb28658cfe
keishimushup <108566690+keishimushup@users.noreply.github.com>
2022-09-19 14:18:27 +0000

    update posts/index レイアウト変更

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,12 +6,12 @@ class Post < ApplicationRecord
   has_many :favorites, dependent: :destroy
 
 
-  def get_image(width, height)
+  def get_image
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpeg')
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-    image.variant(resize_to_limit: [width, height]).processed
+    image
   end
 
   def favorited_by?(user)
